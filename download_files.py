3aabe0815d7e31573369f076e8a8fc7ecab4619c
Anya Sen <anya.sen0810@gmail.com>
2026-08-08 16:26:03 +0100

    including data exploration and file renaming in download code

--- download_files.py
+++ download_files.py
@@ -1,4 +1,4 @@
-# Imports 
+# imports 
 import os
 import pandas as pd
 from google.cloud import storage
@@ -11,25 +11,17 @@ import subprocess
 import json
 import glob
 import shutil
+from itertools import zip_longest
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
 
-import os
-import pandas as pd
-from google.cloud import storage
-import soundfile as sf
-import scipy.io.wavfile as wav
-import numpy as np
-import librosa
-import gc 
-import subprocess 
-import json
-import glob
-import shutil
-
-# making file for data
+# setting up output directory 
 local_data_dir = "data"
 os.makedirs(local_data_dir, exist_ok=True)
 
-# converting annotation file to dataframe
+# loading metadata
+# converting master annotation file to dataframe
 csv_filename = "dclde_2027_dclde_2027_killer_whales_Annotations.csv"
 df = pd.read_csv(csv_filename)
 
@@ -52,63 +44,59 @@ cleaned_df['Subpopulation'] = cleaned_df['Subpopulation'].replace('TKW', 'WCT')
 cleaned_df['Provider'] = cleaned_df['Provider'].replace('UAF_NGOS', 'UAF')
 
 print(cleaned_df.head)
-confound_check = pd.crosstab(cleaned_df['Subpopulation'], 
-                             cleaned_df['Provider'], 
-                             margins = True)
-print(confound_check)
 
-# defining function to retrieve sample rate if librosa fails
-def get_sr_ffprobe(path):
-    result = subprocess.run(
-        ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_streams", path],
-        capture_output=True, text=True
-    )
-    info = json.loads(result.stdout)
-    
-    return int(info["streams"][0]["sample_rate"])
+# functions 
 
-# defining function cluster calls within 10 second window 
+# defining function to cluster calls
 def clustering_calls(calls_df, analysis_window_sec=5.0, max_cluster_span_sec=10.0):
-    
-    # creating empty lists for calls within a cluster and clusters to exclude 
+    # making list of calls in a cluster
     clustered_rows = []
     excluded_clusters = []
 
+    # grouping calls by their soundfile 
     for soundfile, group_df in calls_df.groupby("Soundfile"):
-        # sorting calls in sound file chronologically 
+        # sorting calls in soundfile in chronological order 
         df = group_df.sort_values("FileBeginSec").copy()
-        # defining mid point of cluster
+        # determining midpoint of each call using start and stop times provided in annotation table
         df["midpoint"] = (df["FileBeginSec"] + df["FileEndSec"]) / 2.0
+        # defining 5s window for each call
         df["crop_start"] = df["midpoint"] - analysis_window_sec / 2
         df["crop_end"] = df["midpoint"] + analysis_window_sec / 2
 
+        # determining bounds for cluster
         clusters = []
         current_cluster = [df.iloc[0]]
+        # tracking current clusters crop window 
         current_max_end = df.iloc[0]["crop_end"]
         cluster_span_start = df.iloc[0]["FileBeginSec"]
         cluster_span_end = df.iloc[0]["FileEndSec"]
         
-        # making sure that clusters do not go beyond 10 seconds 
+        # checking each subsequent call against current cluster 
         for i in range(1, len(df)):
             row = df.iloc[i]
+             # calculating what the clusters span would be if this call was added 
             would_be_span_end = max(cluster_span_end, row["FileEndSec"])
             would_be_span = row["FileEndSec"] - cluster_span_start
             
+            # adding call to cluster if maximum span isnt exceeded
             if row["crop_start"] < current_max_end and would_be_span <= max_cluster_span_sec:
                 current_cluster.append(row)
                 current_max_end = max(current_max_end, row["crop_end"])
                 cluster_span_end = would_be_span_end
+            # starting new cluster if maximum span is exceeded
             else:
                 clusters.append(pd.DataFrame(current_cluster))
                 current_cluster = [row]
                 current_max_end = row["crop_end"]
                 cluster_span_start = row["FileBeginSec"]
                 cluster_span_end = row["FileEndSec"]
-                
+        
+        # adding final cluster after checking all calls in file 
         clusters.append(pd.DataFrame(current_cluster))
 
         for cluster_id, cluster_df in enumerate(clusters):
             subpops = cluster_df["Subpopulation"].unique()
+            # excluding clusters that include more than one subpopulation 
             if len(subpops) > 1:
                 print(f"WARNING: {soundfile} cluster {cluster_id} spans multiple "
                       f"subpopulations: {subpops}. Dropping cluster.")
@@ -119,10 +107,9 @@ def clustering_calls(calls_df, analysis_window_sec=5.0, max_cluster_span_sec=10.
                     "num_calls_in_cluster": len(cluster_df)})
                 continue
 
-            # avereraging midpoint of all calls in cluster to get cluster midpoint 
+            # idenitfying mean of cluster by avergaing across midpoints of calls in cluster
             cluster_midpoint = cluster_df["midpoint"].mean()
-
-            # creating dictionary of cluster info
+            # building one row per cluster for new dataframe 
             clustered_rows.append({
                 "Soundfile": soundfile,
                 "Provider": cluster_df["Provider"].iloc[0],
@@ -132,22 +119,95 @@ def clustering_calls(calls_df, analysis_window_sec=5.0, max_cluster_span_sec=10.
                 "cluster_midpoint": cluster_midpoint,
                 "cluster_span_sec": cluster_df["FileEndSec"].max() - cluster_df["FileBeginSec"].min(),
             })
-
+    # returning final clusters and excluded ones in separate dataframes
     return pd.DataFrame(clustered_rows), pd.DataFrame(excluded_clusters)
 
 # removing duplicates 
 deduplicated_df = cleaned_df.drop_duplicates(subset=["Soundfile", "FileBeginSec", "FileEndSec"])
 
-# removing clusters that span over 10 seconds 
+# ensuring that clusters that span over 10 seconds are removed  
 deduplicated_df = deduplicated_df[
     (deduplicated_df["FileEndSec"] - deduplicated_df["FileBeginSec"]) <= 10.0
 ].copy()
 
-# creating dataframe with clusters as rows 
-calls, excluded_df = clustering_calls(deduplicated_df, analysis_window_sec=5.0)
+# defining function to get sampling rate if standard readers (librosa) can't parse the file 
+def get_sr_ffprobe(path):
+    result = subprocess.run(
+        ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_streams", path],
+        capture_output=True, text=True
+    )
+    info = json.loads(result.stdout)
+    
+    return int(info["streams"][0]["sample_rate"])
+
+# creating new dataframe with clusters instead of calls
+calls, excluded = clustering_calls(deduplicated_df, analysis_window_sec=5.0, max_cluster_span_sec=10.0)
+
+# data exploration 
+# checking how distribution of subpopulations across providers
+confound_check = pd.crosstab(calls['Subpopulation'], 
+                             calls['Provider'])
+print(confound_check)
+
+# visualising distribution of subpopulations across providers
+# creating colour mapping for providers 
+providers = sorted(calls["Provider"].unique())
+palette = sns.color_palette("tab20", n_colors=len(providers))
+provider_color_map = dict(zip(providers, palette))
+
+# plotting heatmap of raw counts 
+plt.figure(figsize=(10, 6))
+sns.heatmap(confound_check, annot=True, fmt="d", cmap="YlGnBu", cbar=True, linewidths=0.5)
+plt.title(
+    "Subpopulation vs. Provider (Raw Counts)", fontsize=14, fontweight="bold"
+)
+plt.xlabel("Provider", fontsize=12)
+plt.ylabel("Subpopulation", fontsize=12)
+plt.tight_layout()
+plt.savefig("subpopulation_provider_heatmap.png", dpi=150)
+plt.show()
+
+
+# plotting barchart for proportion representaion 
+confound_prop = (
+    pd.crosstab(
+        calls["Subpopulation"], calls["Provider"], normalize="index")
+    * 100)
+
+# ensure column order matches the provider list used for the color map
+confound_prop = confound_prop[providers]
+bar_colors = [provider_color_map[p] for p in confound_prop.columns]
+
+ax = confound_prop.plot(
+    kind="bar", stacked=True, figsize=(11, 6), color=bar_colors, edgecolor="black"
+)
+plt.title(
+    "Provider Composition per Subpopulation (%)", fontsize=14, fontweight="bold"
+)
+plt.xlabel("Subpopulation", fontsize=12)
+plt.ylabel("Percentage (%)", fontsize=12)
+plt.xticks(rotation=0)
+plt.legend(title="Provider", bbox_to_anchor=(1.05, 1), loc="upper left")
+plt.tight_layout()
+plt.savefig("subpopulation_provider_stacked_bar.png", dpi=150)
+plt.show()
+
+# grouping calls by subpopulation 
+files_by_subpop = {
+                provider: list(group["Soundfile"].unique())
+                for provider, group in calls.groupby("Subpopulation")
+                    }
+
+rotation_order = []
+
+# alternating soundfiles across subpopulations so downloads are balanced if interrupted
+for files_tuple in zip_longest(*files_by_subpop.values()):
+    for f in files_tuple:
+        if f is not None:  
+            rotation_order.append(f)
 
 # grouping calls by the files they belong to 
-calls_by_file = calls.groupby("Soundfile")
+calls_by_file_dict = dict(list(calls.groupby("Soundfile")))
 
 total_num_clusters = len(calls)
 total_num_files = calls["Soundfile"].nunique()
@@ -161,10 +221,14 @@ bucket = client.bucket("noaa-passive-bioacoustic")
 
 # creating column to track future filename - CHANGE TO CALLS FOR FINAL BIT
 calls["ExportFilename"] = [
-    f"{row['Subpopulation']}/clip_{soundfile}_{cluster_id}.wav" for idx, row in calls.iterrows()]
+    f"{row['Subpopulation']}/clip_{row['Soundfile'].replace('.', '_')}_{row['cluster_id']}.wav" 
+    for idx, row in calls.iterrows()
+]
 
 # creating download loop
-for soundfile, group_df in calls_by_file:
+for soundfile in rotation_order:
+    
+    group_df = calls_by_file_dict[soundfile]
     
     # skipping download if file has already been downloaded
     all_clips_exist = True
@@ -193,16 +257,14 @@ for soundfile, group_df in calls_by_file:
     
     
     # searching for a name match anywhere in the file path
-    
     blobs_iterator = client.list_blobs(bucket, prefix=search_dir)
     matching_blobs = [b.name for b in blobs_iterator if base_name in b.name]
     
-    # if no files match
+    # skipping if no match found on cloud 
     if len(matching_blobs) == 0:
         print(f"Could not find match for {soundfile} anywhere under {search_dir}\n")
         continue
-
-    # if more than one file matches
+    # stopping if more than one file matches 
     elif len(matching_blobs) > 1:
         raise ValueError(f"Ambiguous match for {soundfile}: found {matching_blobs}")
 
@@ -212,23 +274,21 @@ for soundfile, group_df in calls_by_file:
     temp_raw_path = None    
     
     try: 
+        # naming temp file to match the cloud file's original extension 
         if matched_blob_path.lower().endswith(".wav"):
             temp_raw_path = os.path.join(local_data_dir, f"temp_{base_name}.wav")
         elif matched_blob_path.lower().endswith(".flac"):
             temp_raw_path = os.path.join(local_data_dir, f"temp_{base_name}.flac")
-        
-        # downloading file if match found
+        # check if file already exists as .wav file
         if temp_raw_path:
             bucket.blob(matched_blob_path).download_to_filename(temp_raw_path)
-
-        # ensuring that download is complete by comparing file sizes  
+        
+        # confiriming download complete before further processing 
         if temp_raw_path and os.path.exists(temp_raw_path):
             blob_obj = bucket.blob(matched_blob_path)
             blob_obj.reload()  
             actual_size = os.path.getsize(temp_raw_path)
             expected_size = blob_obj.size
-
-            # raising error if corrupted download led to incomplete file
             if actual_size != expected_size:
                 raise ValueError(f"Download size mismatch for {soundfile}: "
                                   f"expected {expected_size}, got {actual_size}")
@@ -239,8 +299,7 @@ for soundfile, group_df in calls_by_file:
             try: 
                 info = sf.info(temp_raw_path)
                 samplerate = librosa.get_samplerate(temp_raw_path)
-            
-            # using previously defined function if librosa failed to retrieve sr
+            # falling back to ffprobe if standard readers can't parse the file
             except Exception: 
                 samplerate = get_sr_ffprobe(temp_raw_path)
                 print(f"ffprobe successfully parsed RF64 container. Native rate: {samplerate} Hz")
@@ -308,7 +367,7 @@ for soundfile, group_df in calls_by_file:
     except Exception as e:
         print(f"Error processing {base_name}: {e}")
         
-        # deleting raw file even if processing fails 
+    # deleting raw file even if processing fails 
     finally:
         if temp_raw_path and os.path.exists(temp_raw_path):
             os.remove(temp_raw_path)
@@ -316,8 +375,16 @@ for soundfile, group_df in calls_by_file:
             
 print("Download complete.")
 
-# creating export path for .csv file
+# saving final cluster metadata table 
 metadata_export_path = os.path.join(local_data_dir, "downloaded_calls_metadata.csv")
-# converting dataframe to .csv file
-calls.to_csv(metadata_export_path, index = False) # change to calls for final one
+calls.to_csv(metadata_export_path, index = False) 
 print(f"Saved matched metadata table to {metadata_export_path}")
+
+# renamming clips with file type mentioned twice 
+for root, dirs, files in os.walk(local_data_dir):
+    for fname in files:
+        if fname.endswith(".wav"):
+            new_name = re.sub(r"_(flac|wav)_(\d+)\.wav$", r"_\2.wav", fname)
+            if new_name != fname:
+                os.rename(os.path.join(root, fname), os.path.join(root, new_name))
+                print(f"Renamed {fname} -> {new_name}")
